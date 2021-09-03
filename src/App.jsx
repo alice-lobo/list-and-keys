3eb8edb373c2dc252238ab3ac30549544e0dca8c
Alice <alicelobinha2@gmail.com>
2021-09-02 21:43:21 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,58 @@
 import React from "react";
 
-const listcustomer = [
-    {
-        id:1,
-        name: 'Alice',
-        skills: ['html','css']
-    },
-    {
-        id:2,
-        name: 'Bruno',
-        skills: ['reason','java']
-    },
-    {
-        id:3,
-        name: 'Tobias'
-    },
-    {
-        id:4,
-        name: 'Etelka'
-    }
-]
+const listCustomer = [
+  {
+    id: 1,
+    name: 'Bruno Carneiro',
+    skills: ['React', 'Node', 'CSS', 'Webpack']
+  },
+  {
+    id: 2,
+    name: 'Aline Carneiro',
+    skills: ['HTML', 'React Native', 'Go', 'JS']
+  },
+  {
+    id: 3,
+    name: 'Fulano de Tal',
+    skills: ['Assembly']
+  },
+  {
+    id: 4,
+    name: 'José Ciclano',
+    skills: ['Reason']
+  }
+];
 
 const App = () => {
 
-    const renderCustomers = (customer, index) => {
-        return (
-            <div>
-                <li>{customer.name}</li>
-                {customer.skills.map(renderSkills)}
-            </div>
-            
-        )
-    }
-
-    const renderSkills = (skill, index) => {
-        return (
-            <div key={`skill-${index}`}>
-                <li>{skill}</li>
-            </div>
-            
-            
-        )
-    }
-
+  // eslint-disable-next-line no-unused-vars
+  const renderCustomers = (customer, index) => {
     return (
-        <div>
-            <p>Digital Innovation One</p>
-            <p>Bem-Vindo</p>
-            <div>
-                <ul>
-                {listcustomer.map(renderCustomers)}
-                </ul>
-                <ul>
-                {listcustomer.map(renderSkills)}
-                </ul>
-                
-            </div>
-        </div>
+      <div key={`customer-${customer.id}`}>
+        <li>{customer.name}</li>
+        {customer.skills.map(renderSkills)}
+      </div>
     );
-};
+  };
 
+  const renderSkills = (skill, index) => {
+    return (
+      <div style={{ paddingLeft: '30px' }} key={`skill-${index}`}>
+        <li>{skill}</li>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      <div>
+        <ul>
+          {listCustomer.map(renderCustomers)}
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default App;
